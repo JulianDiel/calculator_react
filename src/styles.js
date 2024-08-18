2fb78a68091694a34138e591c2ae10cb14e1e83d
Julian Diel <drdiel@outlook.com.br>
2024-08-18 19:10:29 -0300

    create calculator reactjs / Calculadora criada em ReactJS

---
+++ src/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #2A293E;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const Content = styled.div`
+    background-color: #2A293E;
+    width: 40%;
+    color:#fff;
+    text-align: center;
+    
+`
+
+export const Row = styled.div`
+    display:flex;
+    flex-direcion: row;
+    justify-content: space-between;
+    align-items: center;
+    
+
+`
+
+export const Column = styled.div`
+    display:flex;
+    flex-direcion: column;
+    justify-content: space-between;
+    align-items: center;
+    
+`
